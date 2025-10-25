c1057aa9479314762b5cf376612dee8daad7426a
moshuiD <68362682+moshuiD@users.noreply.github.com>
2025-10-25 23:13:30 +0800

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "just"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
